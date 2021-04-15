f53a9c22d4913c11739399ded02e6123c1f5db5d
YeongG <yeonggil223@gmail.com>
2021-04-15 23:28:25 +0900

    [MODIFY] DB연결, authRouter연결

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const upload = require("./lib/upload");
+
 const app = express();
+const db = require("./models");
+
+const authRouter = require("./routes/auth");
 
 const exampleJson = JSON.parse(
   fs.readFileSync(__dirname + "/lib/example.json").toString()
@@ -10,10 +14,16 @@ const exampleJson = JSON.parse(
 
 require("dotenv").config();
 
+db.sequelize.sync({ force: false }).then(() => {
+  console.log("DB연결");
+});
+
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/auth", authRouter);
 
 app.post("/analysis", upload.single("image"), (req, res, next) => {
   const uploadFilePath = `${__dirname}/${req.file.path}`;
