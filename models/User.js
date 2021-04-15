e979a4b688eaaa9cdcf8626fc5e1831e59e6087b
YeongG <yeonggil223@gmail.com>
2021-04-15 23:47:10 +0900

    [CREATE] User 테이블 생성

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(seqelize) {
+    return super.init(
+      {
+        id: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "User",
+        tableName: "users",
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    // db.User.hasMany(db.Comm);
+  }
+};
