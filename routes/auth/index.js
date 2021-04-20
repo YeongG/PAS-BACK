26beb67fe6ac17ef7c12ff11a3156b3aaa8b438f
YeongG <yeonggil223@gmail.com>
2021-04-20 23:42:45 +0900

    [MODIFY] POST auth/register 회원가입 구현

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const { checkBody } = require("../../middlewares");
+const { makeHashPassword } = require("../../lib/utils");
+const { User } = require("../../models");
 
 const authRouter = express.Router();
 
-authRouter.post("/register", async (req, res) => {
-  console.log(req.body);
-
-  res.send("");
-});
+authRouter.post(
+  "/register",
+  checkBody(["id", "password", "name"]),
+  async (req, res) => {
+    const { id, password, name } = req.body;
+    const hashPassword = makeHashPassword(password);
+    try {
+      await User.create({ id, password: hashPassword, name });
+      res.status(201).json({ message: "Created Success" });
+      return;
+    } catch (err) {
+      res.status(409).json({ message: "중복된 ID가 존재합니다" });
+    }
+  }
+);
 
 module.exports = authRouter;
